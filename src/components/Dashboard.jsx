935aa5ce418412def00d510258121ab1aca47a69
bell <bellelm@Belkises-MacBook-Air.local>
2025-06-18 19:13:19 +0100

    user can oly vote once

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,34 +1,47 @@
 import { useLocation } from 'react-router-dom';
-import { useState } from 'react';
+import { use, useState } from 'react';
 
 export const Dashboard = () => {
     const location = useLocation()
     const walletAddress = location.state?.walletAddress
-
+  
+    const [votedIndex, setVotedIndex] = useState(null)
+  
     const [proposals, setProposals] = useState([
-        { title: 'Add dark mode', votes: 5 },
-        { title: 'Launch DAO token', votes: 2 },
-        { title: 'Reward contributors', votes: 8 },
-      ]);
-    
-      const voteForProposal = (index) => {
-        const updated = [...proposals];
-        updated[index].votes += 1;
-        setProposals(updated);
-      };
-    
-      return (
-        <div className="dashboard">
-          <h1>Welcome to VoteChain</h1>
-          <p>Connected Wallet: {walletAddress}</p>
-    
-          {proposals.map((p, i) => (
-            <div key={i} className="proposal">
-              <h2>{p.title}</h2>
-              <p>Votes: {p.votes}</p>
+      { title: 'Add dark mode', votes: 5 },
+      { title: 'Launch DAO token', votes: 2 },
+      { title: 'Reward contributors', votes: 8 },
+    ]);
+  
+    const voteForProposal = (index) => {
+      if (votedIndex !== null) return
+  
+      const updated = [...proposals]
+      updated[index].votes += 1
+      setProposals(updated)
+      setVotedIndex(index)
+    };
+  
+    return (
+      <div className="dashboard">
+        <h1>Welcome to VoteChain</h1>
+        <p>Connected Wallet: {walletAddress}</p>
+  
+        {proposals.map((p, i) => (
+          <div key={i} className="proposal">
+            <h2>{p.title}</h2>
+            <p>Votes: {p.votes}</p>
+  
+            {votedIndex === null ? (
               <button onClick={() => voteForProposal(i)}>Vote</button>
-            </div>
-          ))}
-        </div>
-      );
-    }
+            ) : votedIndex === i ? (
+              <p>✅ You voted for this</p>
+            ) : (
+              <p>❌ You cannot vote again</p>
+            )}
+          </div>
+        ))}
+      </div>
+    )
+  }
+  
